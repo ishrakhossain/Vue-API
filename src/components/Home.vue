47f1cb4912e0d8fa334cf7a3877b29de11328dc3
Ishrak <ishrak.hossain789@gmail.com>
2021-01-05 15:38:16 +0600

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="">
+        <table>
+            <tr>
+                <td>Id</td>
+                <td>Name</td>
+                <td>Email</td>
+            </tr>
+            <tr v-for="item in list" v-bind:key="item.id">
+                <td>{{item.id}}</td>
+                <td>{{item.name}}</td>
+                <td>{{item.email}}</td>
+            </tr>
+        </table>
+
+    </div>
+</template>
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+Vue.use(VueAxios, axios)
+export default {
+    name: "Home",
+    data() {
+        return {list: undefined}
+    },
+    mounted() {
+        Vue.axios.get('https://jsonplaceholder.typicode.com/users')
+        .then((res)=>{
+            this.list = res.data
+            console.log(res.data)
+        })
+    }
+}
+</script>
